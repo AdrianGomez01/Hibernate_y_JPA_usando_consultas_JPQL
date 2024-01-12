3ae6bb7e883f8fcc61afdd846e48afd5f3f75bbd
Adrian Gomez <adrian.gomez.garlito@gmail.com>
2024-01-12 08:48:47 +0100

    start

--- src/main/java/bbdd/Main.java
+++ src/main/java/bbdd/Main.java
@@ -1,2 +1,34 @@
-package bbdd;public class Main {
+package bbdd;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            //iniciamos la transacción
+            em.getTransaction().begin();
+
+
+
+
+
+
+        } catch (Exception e) {
+            //Si hay algún problema con al transacción, hacemos un rollback al estado anterior
+            if (em.getTransaction() != null && em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            //Por último cerramos los entity managers
+            em.close();
+            emf.close();
+        }
+    }
 }
